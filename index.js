19e11ce47f038b6677a4ae5310a61fd0d93c23f8
Arif870 <arifuzzaman959@gmail.com>
2021-11-08 19:43:00 +0600

    user seend to db

--- index.js
+++ index.js
@@ -21,7 +21,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfull");
+
+    const database = client.db("DoctorPortal");
+    const appointmentCollection = database.collection("appoinments");
+    const usersCollection = database.collection("users");
+
+    app.get("/appointments", async (req, res) => {
+      const email = req.query.email;
+      const date = req.query.date;
+      const query = { email: email, date: date };
+
+      const cursor = appointmentCollection.find(query);
+      const appoinments = await cursor.toArray();
+      res.json(appoinments);
+    });
+
+    app.post("/appointments", async (req, res) => {
+      const appoinment = req.body;
+      const result = await appointmentCollection.insertOne(appoinment);
+
+      res.json(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
