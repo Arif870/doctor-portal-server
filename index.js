716be61419dd6775c4a701ab494e6a98003adcc3
Arif870 <arifuzzaman959@gmail.com>
2021-11-09 21:10:35 +0600

    authorization by firebase/ npm install firebase

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./doctor-portal-7ea55-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+async function varifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const token = req.headers.authorization.split(" ")[1];
+  }
+  try {
+    const decodedUser = await admin.auth().verifyIdToken(token);
+    req.decodedEmail = decodedUser.email;
+  } catch {}
+  next();
+}
+
 // Middleware
 
 app.use(cors());
@@ -46,6 +65,41 @@ async function run() {
     app.post("/users", async (req, res) => {
       const users = req.body;
       const result = await usersCollection.insertOne(users);
+      res.json(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      let isAdmin = false;
+
+      if (result?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    app.put("/users", async (req, res) => {
+      const users = req.body;
+      const filter = { email: users.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: users };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      res.json(result);
+    });
+
+    app.put("/users/admin", varifyToken, async (req, res) => {
+      const user = req.body.admin;
+      console.log(req.decodedEmail);
+      const filter = { email: user };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
     });
   } finally {
